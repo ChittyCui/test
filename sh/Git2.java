ffeef48094bf55c46370cf76be4733a6f84f2a0f
ChittyCui <821352841@qq.com>
2015-04-08 23:08:09 +0800

    test

---
+++ sh/Git2.java
@@ -0,0 +1,16 @@
+package git;
+
+public class Git2 {
+	 /**
+	 * @param args
+	 */
+	 public static void main(String[] args) {
+	 // TODO Auto-generated method stub
+	 String q = "How are you";
+
+	 String[] aa=(String[]) q.split(" ");
+	 for(int i = aa.length ;i>0 ;i--){
+	 System.out.print(aa[i-1]+" ");
+	 }
+	 }
+	}
